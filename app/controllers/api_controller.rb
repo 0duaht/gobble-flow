55557761aa1560a429539fe6f8d62195a67a1ba9
Tobi Oduah <tobi.oduah@andela.com>
2015-12-09 11:59:40 +0100

    API Access Implements the Gobble API. Allows access from other developers intending to consume the API or the Gobble Gem.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,4 +4,46 @@ class ApiController < ApplicationController
 
   def index
   end
+
+  def create
+    arg_complete = %w(key full_url).all? { |arg| params.include?(arg) }
+    render plain: ENTRY_URL_INVALID && return unless arg_complete
+
+    arg_success_process
+  end
+
+  private
+
+    def arg_success_process
+      @user = User.find_by(api_key: params[:key])
+      render plain: API_INVALID && return unless @user
+
+      process_api_call if short_url_unique?
+    end
+
+    def short_url_unique?
+      render plain: URL_TAKEN &&
+        return if Link.find_by(short_url: params[:short_url])
+      true
+    end
+
+    def process_api_call
+      @link = Link.new(api_params)
+      render plain: ENTRY_INVALID && return unless @link.save
+
+      update_user_and_render
+    end
+
+    def update_user_and_render
+      @link.update user_id: @user.id
+      link_count = @user.link_count + 1
+      @user.update link_count: link_count
+
+      message = LINK_SUCCESS + link_url(@link)
+      render plain: message
+    end
+
+    def api_params
+      params.permit(:full_url, :short_url)
+    end
 end
